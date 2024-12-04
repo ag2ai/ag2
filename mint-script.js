c2c95b99ea8d2b99d348f466991b46feb55193d6
Yusuf Bakır <f.yusufbakir@gmail.com>
2024-12-05 01:13:08 +0300

    styling for design

---
+++ mint-script.js
@@ -0,0 +1,10 @@
+const setTexts = () => {
+  const navigationItems = document.getElementById("navigation-items");
+
+  const navigationH5 = navigationItems.querySelectorAll("h5");
+  for (const h5 of navigationH5) {
+    h5.setAttribute("data-text", h5.textContent);
+  }
+};
+
+setTexts();
