b1d08eb4e0a802d3f2667eed53bc2df38893baaa
Merlinvt <Merlin.von.trott@gmail.com>
2024-12-12 12:09:12 +0100

    fix Orchestrator init.

--- autogen/agentchat/contrib/magentic_one/orchestrator_agent.py
+++ autogen/agentchat/contrib/magentic_one/orchestrator_agent.py
@@ -36,10 +36,7 @@ class OrchestratorAgent(ConversableAgent):
         max_consecutive_auto_reply: Optional[int] = None,
         human_input_mode: Literal["ALWAYS", "NEVER", "TERMINATE"] = "TERMINATE",
         function_map: Optional[Dict[str, Callable]] = None,
-        code_execution_config: Union[Dict, Literal[False]] = {
-            "work_dir": "coding",
-            "use_docker": False,
-        },
+        code_execution_config: Union[Dict, Literal[False]] = False,
         llm_config: Optional[Union[Dict, Literal[False]]] = None,
         default_auto_reply: Union[str, Dict] = "",
         description: Optional[str] = None,
@@ -58,7 +55,6 @@ class OrchestratorAgent(ConversableAgent):
         max_stalls_before_replan: int = 3,
         max_replans: int = 3,
         return_final_answer: bool = False,
-        agent_whole_history: bool = True,
         **kwargs
     ):
         super().__init__(
@@ -68,7 +64,7 @@ class OrchestratorAgent(ConversableAgent):
             max_consecutive_auto_reply=max_consecutive_auto_reply,
             human_input_mode=human_input_mode,
             function_map=function_map,
-            code_execution_config=False,
+            code_execution_config=code_execution_config,
             llm_config=llm_config,
             default_auto_reply=default_auto_reply,
             description=description,
