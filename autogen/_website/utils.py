2b99b2d95edf41eec50a04a62c395aa301e6ae03
Harish Mohan Raj <harish@airt.ai>
2025-02-11 17:40:19 +0530

    Update documentation contribution instructions

--- autogen/_website/utils.py
+++ autogen/_website/utils.py
@@ -20,8 +20,9 @@ def get_git_tracked_and_untracked_files_in_directory(directory: Path) -> set[Pat
 
 def copy_only_git_tracked_and_untracked_files(src_dir: Path, dst_dir: Path) -> None:
     """Copy only the files that are tracked by git or newly added from src_dir to dst_dir."""
-    git_tracked_files_in_website_dir = get_git_tracked_and_untracked_files_in_directory(src_dir)
-    for src in git_tracked_files_in_website_dir:
+    tracked_and_new_files = get_git_tracked_and_untracked_files_in_directory(src_dir)
+
+    for src in tracked_and_new_files:
         if src.is_file():
             dst = dst_dir / src.relative_to(src_dir)
             dst.parent.mkdir(parents=True, exist_ok=True)
