cd99d6dfab12d80dad573fe2ec009b9e49ea36de
Davor Runje <zlikowski@gmail.com>
2025-03-07 09:03:43 +0100

    wip

--- autogen/llm_config.py
+++ autogen/llm_config.py
@@ -2,44 +2,100 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from abc import ABC, abstractmethod
 from contextvars import ContextVar
-from enum import Enum
-from typing import Any, Optional
+from typing import TYPE_CHECKING, Annotated, Any, Literal, Optional
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
 
-current_llm_config: ContextVar[dict[str, list[dict[str, Any]]]] = ContextVar("current_llm_config")
+if TYPE_CHECKING:
+    from .oai.client import ModelClient
+
+# class LLMProvider(str, Enum):
+#     openai = "openai"
+#     bedrock = "bedrock"
+#     anthropic = "anthropic"
+#     cerebras = "cerebras"
+#     cohere = "cohere"
+#     deepseek = "deepseek"
+#     google = "google"
+#     groq = "groq"
+#     mistral = "mistral"
+#     ollama = "ollama"
+#     together = "together"
+
+LLMProvider = Literal[
+    "openai",
+    "bedrock",
+    "anthropic",
+    "cerebras",
+    "cohere",
+    "deepseek",
+    "google",
+    "groq",
+    "mistral",
+    "ollama",
+    "together",
+]
 
 
-class LLMProvider(str, Enum):
-    openai = "openai"
-    bedrock = "bedrock"
-    anthropic = "anthropic"
-    cerebras = "cerebras"
-    cohere = "cohere"
-    deepseek = "deepseek"
-    google = "google"
-    groq = "groq"
-    mistral = "mistral"
-    ollama = "ollama"
-    together = "together"
-
-
-class IndividualLLMConfig(BaseModel):
-    api_type: LLMProvider = LLMProvider.openai
-    model: str
-    api_key: str
-    base_url: Optional[HttpUrl] = None
-    tags: Optional[list[str]] = None
+# class IndividualLLMConfig(BaseModel):
+#     api_type: LLMProvider = "openai"
+#     model: str
+#     api_key: str
+#     base_url: Optional[HttpUrl] = None
+#     tags: Optional[list[str]] = None
 
 
 class LLMConfig(BaseModel):
-    config_list: list[IndividualLLMConfig]
+    # class variable not touched by BaseModel
+    _current_llm_config: ContextVar[dict[str, list[dict[str, Any]]]] = ContextVar("current_llm_config")
+
+    # used by BaseModel to create instance variables
+    config_list: Annotated[list["LLMConfigEntry"], Field(default_factory=list)]
+    temperature: Optional[float] = None
+    check_every_ms: Optional[int] = None
+    max_new_tokens: Optional[int] = None
+    seed: Optional[int] = None
+    allow_format_str_template: Optional[bool] = None
+    response_format: Optional[str] = None
+    timeout: Optional[int] = None
+    cache_seed: Optional[int] = None
 
     def __enter__(self):
         # Store previous context and set self as current
-        self._token = current_llm_config.set(self.model_dump_json())
+        self._token = LLMConfig._current_llm_config.set(self.model_dump_json())
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        current_llm_config.reset(self._token)
+        LLMConfig._current_llm_config.reset(self._token)
+
+
+class LLMConfigEntry(BaseModel, ABC):
+    api_type: LLMProvider
+    model: str
+    api_key: Optional[str] = None
+    base_url: Optional[HttpUrl] = None
+    tags: Annotated[list[str], Field(default_factory=list)]
+
+    @property
+    @abstractmethod
+    def create_client(self) -> "ModelClient": ...
+
+
+class OpenAILLMConfigEntry(LLMConfigEntry):
+    api_type: LLMProvider = "openai"
+
+    def create_client(self) -> "ModelClient":
+        raise NotImplementedError
+
+
+class AzureOpenAILLMConfigEntry(LLMConfigEntry):
+    api_type: LLMProvider = "openai"
+
+    def create_client(self) -> "ModelClient":
+        raise NotImplementedError
+
+
+class GeminiLLMConfigEntry(LLMConfigEntry):
+    api_type: LLMProvider = "openai"
--- test/test_llm_config.py
+++ test/test_llm_config.py
@@ -4,47 +4,58 @@
 
 import pytest
 
-from autogen.llm_config import IndividualLLMConfig, LLMConfig, LLMProvider, current_llm_config
+from autogen.llm_config import OpenAILLMConfigEntry
+from test.conftest import Credentials
+
+# def test_llm_provider():
+#     assert LLMProvider.openai == "openai"
+#     assert LLMProvider.bedrock == "bedrock"
+#     assert LLMProvider.anthropic == "anthropic"
+#     assert LLMProvider.cerebras == "cerebras"
+#     assert LLMProvider.cohere == "cohere"
+#     assert LLMProvider.deepseek == "deepseek"
+#     assert LLMProvider.google == "google"
+#     assert LLMProvider.groq == "groq"
+#     assert LLMProvider.mistral == "mistral"
+#     assert LLMProvider.ollama == "ollama"
+#     assert LLMProvider.together == "together"
 
 
-def test_llm_provider():
-    assert LLMProvider.openai == "openai"
-    assert LLMProvider.bedrock == "bedrock"
-    assert LLMProvider.anthropic == "anthropic"
-    assert LLMProvider.cerebras == "cerebras"
-    assert LLMProvider.cohere == "cohere"
-    assert LLMProvider.deepseek == "deepseek"
-    assert LLMProvider.google == "google"
-    assert LLMProvider.groq == "groq"
-    assert LLMProvider.mistral == "mistral"
-    assert LLMProvider.ollama == "ollama"
-    assert LLMProvider.together == "together"
+# def test_individual_llm_config():
+#     individual_llm_config = LLMConfigEntry(api_type=LLMProvider.openai, model="model", api_key="api_key")
+#     assert individual_llm_config.api_type == LLMProvider.openai
+#     assert individual_llm_config.model == "model"
+#     assert individual_llm_config.api_key == "api_key"
 
 
-def test_individual_llm_config():
-    individual_llm_config = IndividualLLMConfig(api_type=LLMProvider.openai, model="model", api_key="api_key")
-    assert individual_llm_config.api_type == LLMProvider.openai
-    assert individual_llm_config.model == "model"
-    assert individual_llm_config.api_key == "api_key"
+# def test_llm_config():
+#     llm_config = LLMConfig(
+#         config_list=[LLMConfigEntry(api_type=LLMProvider.openai, model="model", api_key="api_key")]
+#     )
+#     assert llm_config.config_list[0].api_type == LLMProvider.openai
+#     assert llm_config.config_list[0].model == "model"
+#     assert llm_config.config_list[0].api_key == "api_key"
+#     assert llm_config.config_list[0].base_url is None
+#     assert llm_config.config_list[0].tags is None
 
 
-def test_llm_config():
-    llm_config = LLMConfig(
-        config_list=[IndividualLLMConfig(api_type=LLMProvider.openai, model="model", api_key="api_key")]
-    )
-    assert llm_config.config_list[0].api_type == LLMProvider.openai
-    assert llm_config.config_list[0].model == "model"
-    assert llm_config.config_list[0].api_key == "api_key"
-    assert llm_config.config_list[0].base_url is None
-    assert llm_config.config_list[0].tags is None
+# def test_current_llm_config():
+#     llm_config = LLMConfig(
+#         config_list=[LLMConfigEntry(api_type=LLMProvider.openai, model="model", api_key="api_key")]
+#     )
+#     with llm_config:
+#         print(current_llm_config.get())
+#         assert current_llm_config.get() == llm_config.model_dump_json()
+#     with pytest.raises(LookupError):
+#         current_llm_config.get()
 
 
-def test_current_llm_config():
-    llm_config = LLMConfig(
-        config_list=[IndividualLLMConfig(api_type=LLMProvider.openai, model="model", api_key="api_key")]
-    )
-    with llm_config:
-        print(current_llm_config.get())
-        assert current_llm_config.get() == llm_config.model_dump_json()
-    with pytest.raises(LookupError):
-        current_llm_config.get()
+class TestLLMConfig:
+    @pytest.fixture
+    def openai_llm_config_entry(self, mock_credentials: Credentials) -> OpenAILLMConfigEntry:
+        return OpenAILLMConfigEntry(model="gpt-4o-mini", api_key=mock_credentials.api_key)
+
+    def test_serialization(self, openai_llm_config_entry: OpenAILLMConfigEntry):
+        actual = openai_llm_config_entry.model_dump()
+        expected = {"api_type": "openai", "model": "gpt-4o-mini", "api_key": "mock_api_key"}
+        assert actual == expected
