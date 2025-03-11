b75d861117305dbdb29188c76e52a155cf83c515
Eric-Shang <sencourant@outlook.com>
2025-03-11 04:04:53 +0000

    Change name

--- autogen/agentchat/contrib/rag/__init__.py
+++ autogen/agentchat/contrib/rag/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .chromadb_query_engine import ChromaDBQueryEngine
-from .llama_index_query_engine import LlamaIndexQueryEngine
+from .llamaindex_query_engine import LlamaIndexQueryEngine
 from .mongodb_query_engine import MongoDBQueryEngine
 from .query_engine import RAGQueryEngine
 
