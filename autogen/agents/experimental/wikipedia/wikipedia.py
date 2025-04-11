f52f4037a4807982aec27ee0e2078f7bb0d15883
Maruf Aytekin <aaytekin@gmail.com>
2025-04-11 00:00:38 -0400

    init wikipedia agent

---
+++ autogen/agents/experimental/wikipedia/wikipedia.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .... import ConversableAgent
+from ....doc_utils import export_module
+
+
+@export_module("autogen.agents.experimental")
+class WikipediaAgent(ConversableAgent):
+    pass
