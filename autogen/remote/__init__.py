abd2a307adf75ff36d735562564dd7540ab3bf8f
Nikita Pastukhov <diementros@yandex.ru>
2025-08-20 19:38:08 +0300

    chore: apply ruff c4 rule

---
+++ autogen/remote/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .agent import RemoteAgent
+from .runtime import AgentBus
+
+__all__ = (
+    "AgentBus",
+    "RemoteAgent",
+)
