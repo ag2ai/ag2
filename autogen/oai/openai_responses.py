27c5ee1cce9a4d524309b01e477856ddd39d999b
Priyanshu Yashwant Deshmukh <priyanshudeshmukh4@gmail.com>
2025-12-11 17:10:15 +0530

    fix: operation type validation

--- autogen/oai/openai_responses.py
+++ autogen/oai/openai_responses.py
@@ -310,9 +310,6 @@ class OpenAIResponsesClient:
         editor = WorkspaceEditor(workspace_dir=workspace_dir, allowed_paths=allowed_paths)
         
 
-        # Valid operation types for apply_patch tool
-        valid_operations = {"create_file", "update_file", "delete_file"}
-
         editor = WorkspaceEditor(workspace_dir=workspace_dir, allowed_paths=allowed_paths)
         op_type = operation.get("type")
 
