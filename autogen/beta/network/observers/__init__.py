fbcd6e14aa820923c20fe2ed5133cabe11857661
SirEntropy <rickchen19970829@outlook.com>
2026-04-13 08:42:04 -0700

    remove: remove previous design implementations

--- autogen/beta/network/observers/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 - 2026, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
-"""Re-export observers from their promoted location in autogen.beta.observers."""
-
-from autogen.beta.observers import LoopDetector, TokenMonitor
-
-__all__ = ["LoopDetector", "TokenMonitor"]
--- autogen/beta/network/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 - 2026, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
-from .rate_limiter import RateLimiter
-from .telemetry import TelemetryPlugin
-from .topic import TopicPlugin
-
-__all__ = ("RateLimiter", "TelemetryPlugin", "TopicPlugin")
